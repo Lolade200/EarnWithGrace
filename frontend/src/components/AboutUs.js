a0a265a280a50ef589a8e7e3fd71c51f95735fc1
Lolade200 <146335666+Lolade200@users.noreply.github.com>
2026-09-11 11:28:13 +0100

    Fix import statement for Header component

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import "./AboutUs.css";
-import Header2 from "../components/Header2"; // Adjust path based on your folder structure
+import Header from ".Header2"; // Adjust path based on your folder structure
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPollH,
