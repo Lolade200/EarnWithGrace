db1e7894aaef108f6a0b9e342c510919f9833f5a
Lolade200 <146335666+Lolade200@users.noreply.github.com>
2026-09-11 11:01:00 +0100

    jfjdkj

--- frontend/src/components/Surveys.js
+++ frontend/src/components/Surveys.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Surveys.css";
-import Header2 from "../components/Header2";
+import Header from "../components/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPollH,
